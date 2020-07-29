f0fc55fd26a5362a7167c66086955f9ef9ccdc74
spwebler <66193376+spwebler@users.noreply.github.com>
2020-07-29 10:59:59 +0530

    Create Day3.sol

---
+++ Day3.sol
@@ -0,0 +1,36 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+/**
+ * @title Owner
+ * @dev Set & change owner
+ */
+contract Owner {
+
+    address private owner;
+    
+    // event for EVM logging
+    event OwnerSet(address indexed oldOwner, address indexed newOwner);
+    
+    // modifier to check if caller is owner
+    modifier isOwner() {
+        require(msg.sender == owner, "Caller is not owner");
+        _;
+    }
+    
+
+    constructor() public {
+        owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
+        emit OwnerSet(address(0), owner);
+    }
+
+
+    function changeOwner(address newOwner) public isOwner {
+        emit OwnerSet(owner, newOwner);
+        owner = newOwner;
+    }
+
+ 
+    function getOwner() external view returns (address) {
+        return owner;
+    }
+}
